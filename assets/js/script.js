668015e49358055ef312a5981c2c8356e855d592
mrxanthic <mrxanthic@gmail.com>
2022-03-20 16:17:18 -0500

    added js var and arrays, changed some ids

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+var timer = document.querySelector("#timer");
+var startQuiz = document.querySelector("#startQuiz");
+var questionsDiv = document.querySelector("#questionsDiv")
+var timeLeft = 60;
+var penalty = 10;
+var currentTime = 0;
+
+var questionsArray = [
+    {
+        ask: "placeholder",
+        choices: ["aasd", "basd", "cdasf", "asd"],
+        answer: "aasd"
+    },
+    {
+        ask: "placeholder",
+        choices: ["aasd", "basd", "cdasf", "asd"],
+        answer: "aasd"
+    },
+    {
+        ask: "placeholder",
+        choices: ["aasd", "basd", "cdasf", "asd"],
+        answer: "aasd"
+    },
+    {
+        ask: "placeholder",
+        choices: ["aasd", "basd", "cdasf", "asd"],
+        answer: "aasd"
+    },
+    {
+        ask: "placeholder",
+        choices: ["aasd", "basd", "cdasf", "asd"],
+        answer: "aasd"
+    },
+    {
+        ask: "placeholder",
+        choices: ["aasd", "basd", "cdasf", "asd"],
+        answer: "aasd"
+    },
+];
