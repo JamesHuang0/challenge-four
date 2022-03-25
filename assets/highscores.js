7d7517615cde55ede7b94f661571ddf7f6de73c6
mrxanthic <mrxanthic@gmail.com>
2022-03-24 20:01:19 -0500

    fixed link

--- assets/highscores.js
+++ assets/highscores.js
@@ -14,5 +14,5 @@ if (allScores !== null) {
 }
 
 backBtn.addEventListener("click", function () {
-    window.location.href="mrxanthic.github.io/challenge-four/"
+    window.location.replace="https://mrxanthic.github.io/challenge-four/"
 });
