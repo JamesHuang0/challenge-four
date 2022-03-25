b8e75fc9d09601997f2393f6dee00fb54c39454a
mrxanthic <mrxanthic@gmail.com>
2022-03-24 19:15:23 -0500

    added highscores js and html

---
+++ assets/highscores.js
@@ -0,0 +1,18 @@
+var highScore = document.querySelector("#highScore");
+var backBtn = document.querySelector("backBtn");
+
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+    
+    for (var i = 0; i <allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].name + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+    }
+}
+
+backBtn.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
